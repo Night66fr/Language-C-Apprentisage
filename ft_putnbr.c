f37fc19354d96670ae154d0aa5a4915f831c5440
Stan <158843492+Night66fr@users.noreply.github.com>
2026-04-09 10:59:40 +0200

    refactor

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -1,28 +1,24 @@
 #include <stdio.h>
 
-void afficher_chiffres(int n) {
-    int i;
-    int dizaine;
-    int unite;
-    int centaine;
-    for (i = 0; i <= 999; i++) {
-        centaine = i / 100;
-        dizaine = (i / 10) % 10;
-        unite = i % 10;
-        putchar(centaine + '0');
-        putchar(dizaine + '0');
-        putchar(unite + '0');
-        if (i < 999) {
+void calcul_digits(int n) {
+    putchar((n / 1000) + '0');
+    putchar(((n / 100) % 10)+ '0');
+    putchar(((n / 10) % 10) + '0');
+    putchar((n % 10) + '0');
+}
+
+void calcul_compteur(int maximum) {
+    for (int i = 0; i <= 9999; i++) {
+        calcul_digits(i);
+        if (i < maximum) {
             putchar(',');
             putchar(' ');
         }
-    }    
+    }
 }
 
-
 int main() {
-    int i;
-    afficher_chiffres(i);
+    calcul_compteur(9999);
     putchar('\n');
     return 0;
 }
